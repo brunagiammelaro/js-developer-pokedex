2aea463855d52acc9b5e8f51d593a6c73eefe7b3
brunagiammelaro <138893476+brunagiammelaro@users.noreply.github.com>
2023-08-27 17:11:14 -0300

    Finalização do projeto

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Trilha JS Developer - Pokedex
+
+   # Meu Projeto - Primeiras Páginas Interativas com JavaScript
+
+Este projeto faz parte do desafio do capítulo "Primeiras Páginas Interativas com JavaScript" do curso de Fullstack Java + Angular da Digital Innovation One (DIO). O objetivo do desafio era continuar a página criada em aula usando recursos da API.
+
+A página criada foi inspirada na seguinte imagem:
+
+![Pokedex App](https://cdn.dribbble.com/users/1171520/screenshots/6540871/pokedex2.png)
+https://dribbble.com/shots/6540871-Pokedex-App/attachments/6540871-Pokedex-App?mode=media
+
+Assim como os seguintes projetos de colegas de curso:
+
+- [Projeto de Anderson Amantino](https://anderson-mntn-js-developer-pokedex-gilt.vercel.app/): Inspirou-nos com a abordagem única de interatividade e design da Pokedex.
+
+- [Projeto de Erica Lima e Silva](https://spiky-leg.surge.sh/): Fomos influenciados pela criatividade e pelas soluções de design apresentadas no projeto da Erica.
+
+Agradeço a Anderson Amantino e Erica Lima e Silva por compartilharem seus projetos e nos inspirarem a melhorar nosso trabalho.
+
+
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,16 @@
 const pokemonList = document.getElementById('pokemonList')
 const loadMoreButton = document.getElementById('loadMoreButton')
 
+
 const maxRecords = 151
 const limit = 10
 let offset = 0;
 
+
 function convertPokemonToLi(pokemon) {
     return `
-        <li class="pokemon ${pokemon.type}">
-            <span class="number">#${pokemon.number}</span>
+        <li class="pokemon ${pokemon.type}" onClick="selectPokemon(${pokemon.number})">
+            <span class="number">#${pokemon.number.toString().padStart(3,'0')}</span>
             <span class="name">${pokemon.name}</span>
 
             <div class="detail">
@@ -18,9 +20,96 @@ function convertPokemonToLi(pokemon) {
 
                 <img src="${pokemon.photo}"
                      alt="${pokemon.name}">
+                     
             </div>
+
         </li>
+     
     `
+}
+
+const selectPokemon = async (id) =>{
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`
+    const res = await fetch(url)
+    const pokemon = await res.json()
+    displayPopup(pokemon)
+}
+
+const displayPopup = (pokemon) =>{
+   
+    const types = pokemon.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types
+
+    pokemon.types = types
+    pokemon.type = type
+ 
+    const photo = pokemon.sprites.other.dream_world.front_default
+    const htmlString = `
+   
+    <div id="popup">
+      <div id="detailPokemon">
+          <button id="closeBtn" onClick="closePopup()">Fechar</button>
+            <li class="pokemon ${pokemon.type}">
+            <span class="name">${pokemon.name}</span>
+            <span class="number">#${pokemon.id.toString().padStart(3,"0")}</span>
+
+            <div class="detail">
+                <ol class="types">
+                    ${pokemon.types.map((type) =>`<li class="type ${type}">${type}</li>`).join('')}
+                </ol>
+            </div>
+                 <img id="img-pokemon" src="${photo}"alt="${pokemon.name}">
+                 <div id="data">
+                 <div class="tabs">
+                     <button class="tab" data-tab="tab1">Status</button>
+                     <button class="tab" data-tab="tab2">Atributos</button>
+                 </div>
+                 <div class="tab-content" id="tab1">
+                 <div id="hability">
+                      <div class="stat-desc">
+                       ${pokemon.stats.map((name_stats) =>`<p class="${type}">${name_stats.stat.name}</p>`).join('')}
+                      </div>
+                      <div class="bar-inner"> ${pokemon.stats.map((base_stats) =>`<p class="${type}">${base_stats.base_stat}</p>`).join('')}</div>
+                      </div>
+                     
+                 </div>
+                 
+                 <div class="tab-content" id="tab2">
+                 <div id="stats">
+                          <div class="stat-bar">
+                              <p>Height: ${(pokemon.height/10).toFixed(2)}m</p>
+                              <p>Weight: ${(pokemon.weight/10)}kg</p>
+                              <p>Main move: ${pokemon.mainmove}</p>
+                              <p>Ability: ${pokemon.abi}</p>
+                            </div>
+                            </div>
+                 </div>
+                 
+                    </div>
+                </div>
+             </div>
+           </div>
+        </div>
+     </li>
+   
+    `
+     pokemonList.innerHTML = htmlString + pokemonList.innerHTML
+
+const defaultTabContent = document.getElementById('tab1');
+    defaultTabContent.style.display = 'block';
+}
+
+const openTab = (tabId) => {
+    const tabContents = document.querySelectorAll('.tab-content');
+    tabContents.forEach(content => {
+        content.style.display = 'none';
+    });
+    document.getElementById(tabId).style.display = 'block';
+}
+
+const closePopup = () =>{
+    const popup = document.getElementById('popup')
+    popup.parentElement.removeChild(popup)
 }
 
 function loadPokemonItens(offset, limit) {
@@ -45,3 +134,10 @@ loadMoreButton.addEventListener('click', () => {
         loadPokemonItens(offset, limit)
     }
 })
+
+document.addEventListener('click', (event) => {
+    if (event.target.classList.contains('tab')) {
+        const tabId = event.target.getAttribute('data-tab');
+        openTab(tabId);
+    }
+});
